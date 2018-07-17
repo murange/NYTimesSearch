f723deb36b33edb01b84406a7c573475e6e2dcac
Samur-Ange <samurange90@gmail.com>
2018-07-17 18:24:16 -0400

    Project finish

---
+++ app/src/main/java/com/codepath/nytimessearch/activities/ArticleActivity.java
@@ -0,0 +1,38 @@
+package com.codepath.nytimessearch.activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.codepath.nytimessearch.Article;
+import com.codepath.nytimessearch.R;
+
+public class ArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Article article = (Article) getIntent().getSerializableExtra("article");
+
+        final WebView webView = (WebView) findViewById(R.id.wvArticle);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                webView.loadUrl(url);
+                return true;
+            }
+        });
+
+        webView.loadUrl(article.getWebUrl());
+    }
+
+}
